ddcda23993465f37031647d406b7c1f2fa348ff4
Craig Banach <craigsb93@gmail.com>
2023-05-31 19:31:19 +0100

    start entries page

--- src/app/entries/page.tsx
+++ src/app/entries/page.tsx
@@ -1,5 +1,22 @@
-const Entries = () => {
-  return <h1>Entries</h1>;
+import { database } from "../utils/database";
+
+const Entries = async () => {
+  const db = await database;
+
+  const categoryData = db.sql`SELECT id, name FROM categories;`;
+  const entries = db.sql`SELECT description, amount FROM entries;`;
+
+  return <div>
+    <h1>Entries</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>description</th>
+          <th>amount</th>
+        </tr>
+      </thead>
+    </table>
+    </div>;
 };
 
 export default Entries;
